29c2415d570f38630031090b385af1dda4815ea7
PAtryk Czarnik <p.czarnik@alx.pl>
2025-06-13 10:28:01 +0200

    first commit

---
+++ src/test/java/com/example/aplikacja_springowa/AplikacjaSpringowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplikacja_springowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjaSpringowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
